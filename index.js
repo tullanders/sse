e8ee9cbb627099a0cf60cacc61d7acb6a4a24bcb
anders <andersbergkvist@hotmail.com>
2023-10-15 01:56:17 +0200

    update

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
 const express = require('express')
 const app = express();
-const router = express.Router();
 const sse = require('./sse');
+const trainAnnouncement = require('./trainannouncement.js');
+
 const port = 3000
 
 app.use(express.static('public'))
 
+// For parsing application/json
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
-app.get('/events/:topic', sse.events);
+});
 
-// create a timer that calls the internalSendEvents function every 5 seconds
+// route event stuff to sse.js
+app.get('/events/:topic', sse.events);
+app.post('/sendevent/:topic', sse.externalSendEvent);
+app.get('/subscribers', sse.subscribers);
+
+trainAnnouncement.fetchTrains();
+
+// Test sending events from this app to topic1.
 setInterval(() => {
-  sse.internalSendEvents({message: 'Hello from topic1!'}, 'topic1');
+  sse.internalSendEvent({message: 'Hello from topic1!'}, 'topic1');
 }, 1600);
 
+// Test sending events from this app to topic2.
 setInterval(() => {
-    sse.internalSendEvents({message: 'Hello from topic2!'}, 'topic2');
+    sse.internalSendEvent({message: 'Hello from topic2!'}, 'topic2');
   }, 3700);
 
 app.listen(port, () => {
--- sse.js
+++ sse.js
@@ -1,38 +1,57 @@
 //npconst cors = require('cors');
 const uuid = require('uuid');
 
-
+// List of subscribers.
 let subscribers = [];
-module.exports.sendEvents = (request, response, next) => {
-  const data = request.body;
-  subscribers.forEach(subscriber => subscriber.response.write(`data: ${JSON.stringify(data)}\n\n`));
 
-  response.json({success: true});
+// Internal function for sending events to subscribers.
+module.exports.internalSendEvent = (data, topic) => {
+  if (subscribers.length == 0) return;
+  selectedSubscribers = subscribers.filter(sub => sub.topic == topic);
+  
+  if (selectedSubscribers.length == 0) return;
+  selectedSubscribers.forEach(s => s.response.write(`data: ${JSON.stringify(data)}\n\n`));
 };
 
-exports.internalSendEvents = (data, topic) => {
-  selectedSubscribers = subscribers.filter(sub => sub.topic === topic);
-  console.log(subscribers.length, selectedSubscribers.length)
-  subscribers.forEach(s => s.response.write(`data: ${JSON.stringify(data)}\n\n`));
-}
+// Endpoint for sending events from external sources. 
+// Uses the internalSendEvent function.
+module.exports.externalSendEvent = (request, response) => {
+  const data = request.body;
+  const topic = request.params.topic;
+  try {
+    module.exports.internalSendEvent(data, topic); 
+    response.send({'status': 'ok', 'data': data, 'topic': topic});
+  }
+  catch (err) {
+    response.send({'status': 'error', 'error': err});
+  };
+  
+};
 
+// Endpoint for subscribing to events.
 module.exports.events = (request, response, next) => {
-  let topic = request.query.topic;
+  let topic = request.params.topic;
   const headers = {
       'Content-Type': 'text/event-stream',
       'Connection': 'keep-alive',
       'Cache-Control': 'no-cache'
    };
+
   const subscriber = {
     id: uuid.v4(),
     topic: topic,
     response
   };
-  
   subscribers.push(subscriber);
+
+  response.writeHead(200, headers);
   
   request.on('close', () => {
-    console.log(`${subscriber.id} Connection closed`);
     subscribers = subscribers.filter(sub => sub.id !== subscriber.id);
   });
 };
+
+// Enndpoint for listing all subscribers.
+module.exports.subscribers = (request, response, next) => {
+  response.send(subscribers.map(sub => {sub.id, sub.topic}));
+}
